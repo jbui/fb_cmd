6f11fb8cb3ae7287d7564e49bfc5254ab91481df
James <j3bui@ucsd.edu>
2012-03-02 20:51:47 -0800

    koala with jamesbuis

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,18 @@
-class RecipesController < ActionController::Base
+require 'koala'
 
-	def post
-	end
+class RecipesController < ActionController::Base
+	before_filter :setup
 
 	def create_event
 	end
 
 	def create_post
+		@graph.put_wall_post("hey, i'm learning kaola")
+	end
+
+	private
+	def setup
+		@graph = Koala::Facebook::API.new('AAADqwRNaOCYBAN79GggRndUpr2DZCS1qPmgY6vorRLLoehoZCWbU7WiXteWRyqHPygiduoWuxcyjy2uLpRL1ZCdMwP8fveEmTFqgDw8kAZDZD')
 	end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ FbCmd::Application.routes.draw do
   match '/auth/facebook/callback' => 'sessions#create'
   match '/auth/failure' => 'sessions#failure'
 
+
+  # routes for recipes
+  match '/api/create_post' => 'recipes#create_post'
+
 end
