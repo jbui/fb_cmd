13672efafb67d42d639216d4a7012bff7446b032
James <j3bui@ucsd.edu>
2012-03-02 18:11:58 -0800

    rails barebones

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbCmd::Application.config.secret_token = '00c44b4ec2f1f71fb61505b4874e511d6adbc763b8263cffbb20a60e10eb94222202bbf8809edb67974a409275063f057c8a1008ae322689ff1183319d629b92'
